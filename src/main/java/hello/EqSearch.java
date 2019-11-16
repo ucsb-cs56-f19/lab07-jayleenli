69e85d49cd4c6655ac1ccd428663dfbd1023d3e4
jayleenli <jayleenli17@gmail.com>
2019-11-15 16:45:32 -0800

    jl - create form and controller/view methods for new the new page

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,25 @@
+package hello;
+
+public class EqSearch {
+   private int distance;
+   private int minmag;
+
+   public EqSearch () {
+   }
+
+   public void setDistance(int distance) {
+      this.distance = distance;
+   }
+
+   public void setMinmag(int minmag) {
+      this.minmag = minmag;
+   }
+   
+   public int getDistance () {
+      return distance;
+   }
+
+   public int getMinmag () {
+      return minmag;
+   }
+}	
